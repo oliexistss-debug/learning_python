f056ba4ff63a0382501184e26107769154125f19
Oliver Wang <oliver.wang.ziyuan@gmail.com>
2026-02-01 18:10:33 +0000

    Add skeleton

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,13 @@
 # Program a binary search function
+
+import random
+
+#numbers = list(range(1, 101))
+#print(numbers)
+
+bill = random.randint(1, 1000)
+low = 1
+high = 1000
+
+print("I'm thinking of a meal bill between £1 and £1000.")
+print("Try to guess it using binary search!")
